c2991b0647c126e8797801ed1b9c4c5608c63e76
dleitch00 <dleitch00@gmail.com>
2025-12-07 09:53:31 -0500

    Created DoMath(), passed in the 2 integer inputs, added them and then printed the result and called the method in main

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -8,7 +8,14 @@ class Program
         int first = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Please enter the second integer");
         int second = Convert.ToInt32(Console.ReadLine());
+        DoMath(first, second);
 
         //Console.WriteLine(first + " " + second);
+    }
+
+    public static void DoMath(int first, int second)
+    {
+        int result = first + second;
+        Console.WriteLine($"{first} + {second} = {result}");
     }
 }
