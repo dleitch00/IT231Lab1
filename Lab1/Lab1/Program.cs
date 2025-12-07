66f295304ab8bfba740efdccb949dfe8e57b66f2
dleitch00 <dleitch00@gmail.com>
2025-12-07 09:33:32 -0500

    Take 2 integer inputs via the console. Save the inputs in the 2 input variables

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Please enter the first integer");
+        int first = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Please enter the second integer");
+        int second = Convert.ToInt32(Console.ReadLine());
+
+        //Console.WriteLine(first + " " + second);
     }
 }
